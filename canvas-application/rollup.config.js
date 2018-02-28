5a070f41110c7b5990bb92562b7ae81669216746
Ben Anderson <banderson@hubspot.com>
2018-02-27 23:01:52 -0500

    Initial commit for canvas-application

---
+++ canvas-application/rollup.config.js
@@ -0,0 +1,60 @@
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const babel = require('rollup-plugin-babel');
+
+const pkg = require('./package.json');
+
+const watch = {
+  include: 'src/**',
+  exclude: 'node_modules/**',
+};
+
+const externals = Object.keys(pkg.dependencies);
+
+module.exports = [
+  // UMD build
+  {
+    input: 'src/index.js',
+    output: {
+      name: 'HubSpotCanvasApplication',
+      format: 'umd',
+      file: pkg.browser,
+      globals: {
+        react: 'React',
+        'create-react-class': 'createClass',
+        'prop-types': 'PropTypes',
+        classnames: 'classnames',
+      },
+    },
+    external: externals,
+    plugins: [
+      resolve(),
+      babel({
+        exclude: ['node_modules/**'],
+        externalHelpers: false,
+      }),
+      commonjs({
+        include: 'node_modules/**',
+      }),
+    ],
+    watch,
+  },
+
+  // CommonJS and ES module
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+    external: externals,
+    plugins: [
+      resolve(),
+      babel({
+        exclude: ['node_modules/**'],
+        externalHelpers: false,
+      }),
+    ],
+    watch,
+  },
+];
